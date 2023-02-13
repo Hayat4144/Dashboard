e27f0925d626bdb5d31ef4c0dba158981a6c3657
Hayat ilyas <ihayat855@gmail.com>
2023-02-13 19:32:48 +0530

    react router is installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard";
 import AsideNavbar from "./global/AsideNavbar";
-
+import Signin from "./auth/Signin";
 
 export default function App() {
   return (
     <Fragment>
-      <Dashboard />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+        </Routes>
+      </Router>
     </Fragment>
   );
 }
