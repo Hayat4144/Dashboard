bacb59d4cdf9f866776ed84361e29193640eff08
Hayat ilyas <ihayat855@gmail.com>
2023-02-14 12:56:22 +0530

    product list is added to dashboard.

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -3,16 +3,18 @@ import AsideNavbar from '../global/AsideNavbar'
 import Cards from './Cards'
 import TransactionSample from './TransactionSample'
 import WelcomeMessage from '../global/WelcomeMessage'
+import ProductList from './Shop/ProductList'
 
 export default function Dashboard() {
     return (
         <Fragment>
             <div className='flex'>
                 <AsideNavbar />
-                <main className='dark:bg-gray-900 w-full h-screen'>
+                <main className='dark:bg-gray-900 w-full h-full'>
                     <WelcomeMessage />
                     <Cards />
                     <TransactionSample />
+                    <ProductList />
                 </main>
             </div>
         </Fragment>
