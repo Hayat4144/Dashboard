2cc6c3a42bd67b87da792635b7b332a10f8262a0
Hayat ilyas <ihayat855@gmail.com>
2023-02-14 09:17:44 +0530

    Greeting mesage is added.

--- src/global/WelcomeMessage.jsx
+++ src/global/WelcomeMessage.jsx
@@ -1,14 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState  ,useEffect} from 'react'
 
 export default function WelcomeMessage() {
+    const [greeting, setGreeting] = useState('')
+    useEffect(() => {
+        let current_time = new Date();
+        if (current_time.getHours() < 12) {
+            setGreeting('Good morning');
+        }
+        else if (current_time.getHours() < 18) {
+            setGreeting('Good afternoon')
+        }
+        else {
+            setGreeting('Good evening')
+        }
+    }, [])
+
+
     return (
         <Fragment>
             <section className='mx-5 my-5 md:mx-10 dark:text-gray-100'>
                 <div className='welcome_message flex space-x-2 text-2xl'>
-                    <h1 className='welcome_text'>Welcome Back,</h1>
+                    <h1 className='welcome_text'>{greeting},</h1>
                     <span className='user_name cursor-pointer'>Hayat ilyas</span>
                 </div>
-                <p>we are glad to see your here again.</p>
+                <p>Here is what happening with your store today.</p>
             </section>
 
         </Fragment>
