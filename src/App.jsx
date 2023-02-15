bcb8d1efeb00b1ca6bd1add9ae532c611586dece
Hayat ilyas <ihayat855@gmail.com>
2023-02-15 14:04:08 +0530

    product component link is added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
-import AsideNavbar from "./global/AsideNavbar";
+import Dashboard from "./Components/Dashboard";;
 import Signin from "./auth/Signin";
 import Signup from "./auth/Signup";
+import AddProducts from "./Components/Shop/AddProducts";
+import ProductList from "./Components/Shop/ProductList";
+import Products from "./Components/Shop/Products";
 
 export default function App() {
   return (
     <Fragment>
       <Router>
         <Routes>
-          <Route path="/" element={<Signin />} />
+          <Route path="/" element={<Dashboard />}/>
+          <Route path="/v3/seller/add/product" element={<AddProducts />} />
+          <Route path="/v3/seller/products" element={<Products />} />
+          <Route path="/3" element={<Signin />} />
           <Route path="/2" element={<Signup />} />
         </Routes>
       </Router>
