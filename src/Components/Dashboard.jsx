df9cce4dbd686dddff1e258906189d2cbf4f9eb3
Hayat ilyas <ihayat855@gmail.com>
2023-02-17 19:55:18 +0530

    navbar is added in dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,16 +1,23 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, lazy, Suspense } from 'react'
 import AsideNavbar from '../global/AsideNavbar'
 import Cards from './Cards'
 import TransactionSample from './TransactionSample'
 import WelcomeMessage from '../global/WelcomeMessage'
 import ProductList from './Shop/ProductList'
+import OrderChart from '../global/OrderChart'
+const MobileNavbar = lazy(()=>import('../global/MobileNavbar'))
 
 export default function Dashboard() {
     return (
         <Fragment>
+            <div className='md:hidden'>
+                <Suspense fallback={'loading...'}>
+                    <MobileNavbar/>
+                </Suspense>
+            </div>
             <div className='flex'>
                 <AsideNavbar />
-                <main className='dark:bg-gray-900 w-full h-full'>
+                <main className='dark:bg-gray-900 h-full w-full'>
                     <WelcomeMessage />
                     <Cards />
                     <TransactionSample />
