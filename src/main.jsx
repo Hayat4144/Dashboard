a4e2cb2dee22d852d07074c08d04d480d4a13210
Hayat ilyas <ihayat855@gmail.com>
2023-02-20 07:38:59 +0530

    add redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,26 @@
 import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import PersistReducer from "./Context/reducers/MainReducer";
+import { createStore } from 'redux'
+
+
+const Store = createStore(
+  PersistReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
+
+let persistor = persistStore(Store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Fragment>
-    <App />
-  </Fragment>,
+  <Provider store={Store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
 )
 
