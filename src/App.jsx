3b8115b8e3942d44bc037c52dcaec7c2fac3c9c3
Hayat ilyas <ihayat855@gmail.com>
2023-03-01 15:16:07 +0530

    Skeleton is completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React, { Fragment, Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard";;
-import AddProducts from "./Components/Shop/AddProducts";
-import ProductList from "./Components/Shop/ProductList";
-import Products from "./Components/Shop/Products";
-import OrderChart from "./global/OrderChart";
-import EditProduct from "./Components/Shop/EditProduct";
 import 'react-loading-skeleton/dist/skeleton.css'
 import SignupSkeleton from "./animation/SignupSkeleton";
 import SinginSkeleton from "./animation/SinginSkeleton";
@@ -16,15 +11,19 @@ import ProfileSkeleton from './animation/ProfileSkeleton'
 import AddressSkeleton from "./animation/AddressSkeleton";
 import PasswordChangeSkeleton from './animation/PasswordChangeSkeleton'
 import EmailSkeleton from './animation/EmailSkeleton'
+import AddProductSkeleton from './animation/AddProductSkeleton'
 const MainAccount = lazy(() => import('./Components/Account/MainAccount'))
 const Signup = lazy(() => import('./auth/Signup'))
 const Signin = lazy(() => import('./auth/Signin'))
 const Orders = lazy(() => import('./Components/Shop/Orders'))
 const Profiles = lazy(() => import('./Components/Account/Profiles'))
 const Address = lazy(() => import('./Components/Account/Address'))
+const EditProduct = lazy(() => import('./Components/Shop/EditProduct'))
 const RequestForgetPassword = lazy(() => import('./Components/Account/RequestForgetPassword'))
 const PasswordChange = lazy(() => import('./Components/Account/PasswordChange'))
+const AddProducts = lazy(() => import('./Components/Shop/AddProducts'))
 const Transactions = lazy(() => import('./Components/Shop/Transactions'))
+const Products = lazy(() => import('./Components/Shop/Products'))
 
 
 export default function App() {
@@ -33,16 +32,16 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/v3/seller/add/product" element={<AddProducts />} />
+          <Route path="/v3/seller/add/product" element={<Suspense fallback={<AddProductSkeleton />}><AddProducts /></Suspense>} />
           <Route path="/v3/seller/account/address" element={<Suspense fallback={<AddressSkeleton />}><Address /></Suspense>} />
-          <Route path="/v3/seller/products" element={<Products />} />
+          <Route path="/v3/seller/products" element={<Suspense fallback={<TransactionSkeleton />}><Products /></Suspense>} />
           <Route path="/v3/seller/account" element={<Suspense fallback={<AccountSkeleton />}><MainAccount /></Suspense>} />
           <Route path="/v3/seller/account/change/password" element={<Suspense fallback={<PasswordChangeSkeleton />}><PasswordChange /></Suspense>} />
           <Route path="/v3/seller/account/request/password/reset" element={<Suspense fallback={<EmailSkeleton />}><RequestForgetPassword /></Suspense>} />
           <Route path="/v3/seller/account/profile" element={<Suspense fallback={<ProfileSkeleton />}><Profiles /></Suspense>} />
           <Route path="/v3/seller/transactions" element={<Suspense fallback={<TransactionSkeleton />}><Transactions /></Suspense>} />
           <Route path="/v3/seller/orders" element={<Suspense fallback={<OrderSkeleton />}><Orders /></Suspense>} />
-          <Route path="/v3/seller/edit/product/:id" element={<EditProduct />} />
+          <Route path="/v3/seller/edit/product/:id" element={<Suspense fallback={<AddProductSkeleton />}><EditProduct /></Suspense>} />
           <Route path="/v3/seller/signin" element={<Suspense fallback={<SinginSkeleton />}><Signin /></Suspense>} />
           <Route path="/v3/seller/signup" element={<Suspense fallback={<SignupSkeleton />}><Signup /></Suspense>} />
         </Routes>
