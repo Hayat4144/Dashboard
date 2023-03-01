03fd0d0fbb4c1b70200d29bec0a9aa9482c1694d
Hayat ilyas <ihayat855@gmail.com>
2023-03-01 12:01:40 +0530

    added dark mode skeleton.

--- src/animation/TableSkeleton.jsx
+++ src/animation/TableSkeleton.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment ,useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import Skeleton from 'react-loading-skeleton'
 
 export default function TableSkeleton() {
@@ -10,19 +10,25 @@ export default function TableSkeleton() {
                 <table className="table-auto w-full">
                     <thead>
                         <tr className="text-justify">
-                            {[1, 2, 3, 4, 5, 6].map((index) => (<th className="px-4 py-2" key={index}><Skeleton className='h-10'/></th>))}
+                            {
+                                [1, 2, 3, 4, 5, 6].map((index) => (
+                                    <th className="px-4 py-2" key={index}>
+                                        <Skeleton className='h-10' baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''}/>
+                                    </th>
+                                ))
+                            }
                         </tr>
                     </thead>
                     <tbody className='mb-2'>
                         {
                             [1, 2, 3, 4, 5, 6].map((index) => (
                                 <tr key={index} className="border-b border-gray-300 dark:border-gray-500">
-                                    <td className="px-4 py-2"><Skeleton  baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
-                                    <td className="px-4 py-2"><Skeleton  baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
-                                    <td className="px-4 py-2"><Skeleton  baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
-                                    <td className="px-4 py-2"><Skeleton  baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
-                                    <td className="px-4 py-2"><Skeleton  baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
-                                    <td className="px-4 py-2"><Skeleton  baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
+                                    <td className="px-4 py-2"><Skeleton baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
+                                    <td className="px-4 py-2"><Skeleton baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
+                                    <td className="px-4 py-2"><Skeleton baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
+                                    <td className="px-4 py-2"><Skeleton baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
+                                    <td className="px-4 py-2"><Skeleton baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
+                                    <td className="px-4 py-2"><Skeleton baseColor={Color} highlightColor={theme === 'dark' ? 'rgba(255, 255, 255, 0.2)' : ''} /></td>
                                 </tr>
                             ))
                         }
