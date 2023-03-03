bb92e70e8ed0ac53e786233fd259ecfcd47c9746
Hayat ilyas <ihayat855@gmail.com>
2023-03-03 08:30:00 +0530

    password change is completed.

--- src/Components/Account/PasswordChange.jsx
+++ src/Components/Account/PasswordChange.jsx
@@ -1,7 +1,10 @@
 import React, { Fragment, useState, Suspense, lazy } from 'react'
 import { Link } from 'react-router-dom'
 import AsideNavbar from '../../global/AsideNavbar'
+import { toastifyoption } from '../../global/Notification'
+import {toast} from 'react-toastify'
 const MobileNavbar = lazy(() => import('../../global/MobileNavbar'))
+
 
 export default function PasswordChange() {
   const [currentpassword, setcurrentpassword] = useState('')
@@ -14,7 +17,7 @@ export default function PasswordChange() {
   // submitHandler 
   const SubmitHandler = async () => {
     setIsLoading(!isLoading)
-    const result = await fetch(`${import.meta.env.DEV ? import.meta.env.VITE_BACKEND_DEV_URL : import.meta.env.VITE_BACKEND_URL}/v3/api/user/change/password`, {
+    const result = await fetch(`${import.meta.env.DEV ? import.meta.env.VITE_BACKEND_DEV_URL : import.meta.env.VITE_BACKEND_URL}/v4/api/seller/change/password`, {
       method: 'PUT',
       headers: {
         "Content-Type": "application/json",
@@ -30,28 +33,10 @@ export default function PasswordChange() {
     setIsLoading(false)
     if (result.status === 200) {
       console.log(data)
-      toast.success(data.data, {
-        position: "bottom-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      });
+      toast.success(data.data, toastifyoption);
     }
     else {
-      toast.error(data.error, {
-        position: 'bottom-center',
-        autoClose: 5000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      })
+      toast.error(data.error, toastifyoption)
     }
   }
 
@@ -60,9 +45,6 @@ export default function PasswordChange() {
   }
   const NewPasswordChange = (e) => {
     setNewpassword(e.target.value)
-  }
-  const ConfrimPasswordChange = (e) => {
-    setConfirmpassword(e.target.value)
   }
   return (
     <Fragment>
@@ -80,40 +62,64 @@ export default function PasswordChange() {
           md:px-5 md:mx-auto md:w-4/6 lg:w-2/5  mt-4 xl:mx-auto  lg:mx-auto 
           md:my-10'>
             <form
-              onChange={(e) => {
+              onSubmit={(e) => {
                 e.preventDefault();
                 SubmitHandler();
               }}
             >
               <div className='old_password'>
                 <label htmlFor="old_password">Old Password</label>
-                <input type="password" id="old_password"
+                <input
+                  type={`${showPassword1 ? 'text' : 'password'}`}
+                  id="old_password"
                   value={currentpassword}
                   onChange={CurrentPasswordChange}
                   className="px-2 py-2 outline-none rounded-md border border-gray-400 text-sm 
                   focus:shadow-md dark:bg-inherit dark:focus:border-gray-400 focus:border-indigo-800
                   my-1 w-full dark:placeholder:text-gray-200 dark:text-gray-200 placeholder:text-gray-500"
                   placeholder='Enter your old password' />
+                <span
+                  className='text-sm mr-1 cursor-pointer relative float-right bottom-9 
+                  right-1' onClick={(e) => {
+                    setshowpassword1(!showPassword1)
+                  }}> {showPassword1 ? ' hide' : 'show'}
+                </span>
               </div>
               <div className='new_password my-2'>
                 <label htmlFor="new_password">New Password</label>
-                <input type="password" id="new_password"
+                <input
+                  type={`${showPassword2 ? 'text' : 'password'}`}
+                  id="new_password"
                   value={newpassword}
                   onChange={NewPasswordChange}
                   className="px-2 py-2 outline-none rounded-md border border-gray-400 text-sm 
                   focus:shadow-md dark:bg-inherit dark:focus:border-gray-400 focus:border-indigo-800
                   my-1 w-full dark:placeholder:text-gray-200 dark:text-gray-200 placeholder:text-gray-500"
                   placeholder='Enter your new password' />
+                <span
+                  className='text-sm mr-1 cursor-pointer relative float-right bottom-9 
+                  right-1' onClick={(e) => {
+                    setshowpassword2(!showPassword2)
+                  }}> {showPassword2 ? ' hide' : 'show'}
+                </span>
               </div>
               <div className='confirm_password my-2'>
                 <label htmlFor="confirm_password">Confirm Password</label>
-                <input type="password" id="confirm_password"
-                  value={newpassword}
-                  onChange={NewPasswordChange}
+                <input
+                  type={`${showPassword3 ? 'text' : 'password'}`}
+                  id="confirm_password"
+                  value={confirmpassword}
+                  onChange={(e) => setConfirmpassword(e.target.value)}
                   className="px-2 py-2 outline-none rounded-md border border-gray-400 text-sm 
                   focus:shadow-md dark:bg-inherit dark:focus:border-gray-400 focus:border-indigo-800
                   my-1 w-full dark:placeholder:text-gray-200 dark:text-gray-200 placeholder:text-gray-500"
                   placeholder='confirm your password' />
+                <span
+                  className='text-sm mr-1 cursor-pointer relative float-right bottom-9 
+                  right-1' onClick={(e) => {
+                    setshowpassword3(!showPassword3)
+                  }}> {showPassword3 ? ' hide' : 'show'}
+                </span>
               </div>
               <button
                 type='submit'
